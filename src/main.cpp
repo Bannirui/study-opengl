6d24d01e7fd02d22cec4169c236d7e8e631a615f
dingrui <Bannirui@outlook.com>
2025-06-30 16:49:45 +0800

    多个立方体

--- src/main.cpp
+++ src/main.cpp
@@ -154,20 +154,35 @@ void render(Shader shader)
     // 告诉GPU绘制图形用的VAO
     glBindVertexArray(vao);
     // 转换坐标
-    glm::mat4 model = glm::mat4(1.0f);
     glm::mat4 view = glm::mat4(1.0f);
-    glm::mat4 projection = glm::mat4(1.0f);
-    model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
     view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+    shader.setMat4("view", view);
+    glm::mat4 projection = glm::mat4(1.0f);
     projection = glm::perspective(glm::radians(45.0f), (float)app->getWidth() / (float)app->getHeight(), 0.1f, 100.0f);
-    // 开辟uniform全局变量给vertex shader
-    unsigned int modelLoc = glGetUniformLocation(shader.m_ID, "model");
-    unsigned int viewLoc = glGetUniformLocation(shader.m_ID, "view");
-    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
     shader.setMat4("projection", projection);
-    // 向GPU发送绘制指令
-    GL_CALL_AND_CHECK_ERR(glDrawArrays(GL_TRIANGLES, 0, 36));
+    // 开辟uniform全局变量给vertex shader
+    // 多个立方体的位置
+    glm::vec3 positions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3( 1.3f, -2.0f, -2.5f),
+        glm::vec3( 1.5f,  2.0f, -2.5f),
+        glm::vec3( 1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
+    for (unsigned int i = 0, sz=sizeof(positions); i < sz / sizeof(positions[0]); i++) {
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, positions[i]);
+        float angle = 20.0f * i;
+        model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+        shader.setMat4("model", model);
+        // 向GPU发送绘制指令
+        GL_CALL_AND_CHECK_ERR(glDrawArrays(GL_TRIANGLES, 0, 36));
+    }
 }
 
 int main()
