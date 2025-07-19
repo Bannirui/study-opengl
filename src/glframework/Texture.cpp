2628462510d02cf0f0aa05cf27f465867fbea900
dingrui <Bannirui@outlook.com>
2025-07-19 10:09:13 +0800

    mipmap

--- src/glframework/Texture.cpp
+++ src/glframework/Texture.cpp
@@ -29,17 +29,17 @@ Texture::Texture(const std::string& path, unsigned int unit) : m_Uint(unit)
     // 开辟显存 传输数据
     // level mipmap的层级 如果这个api调用的时候level传入了非0的值 就意味着默认要开启opengl的mipmap功能 就一定要传给opengl图片大小一直到1*1大小 如果没有一直传入图片 就会报错
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, m_Width, m_Height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    // 启用opengl的mipmap功能 给gl状态机插槽GL_TEXTURE_2D上的图片生成mipmap
+    glGenerateMipmap(GL_TEXTURE_2D);
     stbi_image_free(data);
     // 纹理包裹 当UV坐标超出0 1怎么处理
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     // 纹理过滤器
-    // 需要像素<图片像素 使用Nearest
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    // 需要像素<图片像素 使用Nearest 当物体远去变小的时候介于2个mipmap之间 让opengl用mipmap的插值图片
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
     // 需要像素>图片像素 使用Linear
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    // 启用opengl的mipmap功能
-    glGenerateMipmap(GL_TEXTURE_2D);
 }
 
 Texture::~Texture()
