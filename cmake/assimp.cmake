023eab1296282396fffd1b1edfcedbe249f6caea
dingrui <Bannirui@outlook.com>
2025-07-28 22:49:38 +0800

    整合Assimp库

---
+++ cmake/assimp.cmake
@@ -0,0 +1,7 @@
+include(FetchContent)
+FetchContent_Declare(
+        assimp
+        GIT_REPOSITORY	https://github.com/Bannirui/assimp.git
+        GIT_TAG 	    59bc03d931270b6354690512d0c881eec8b97678
+)
+FetchContent_MakeAvailable(assimp)
